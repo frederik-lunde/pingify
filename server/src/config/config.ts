5b8bbcdb13aff139f0e15a65cff02d3ad20aa656
Frederik Lunde <frederik.a.lunde@gmail.com>
2025-03-11 15:39:55 +0100

    typo

--- server/src/config/config.ts
+++ server/src/config/config.ts
@@ -5,5 +5,5 @@ dotenv.config();
 export const config = {
   port: process.env.PORT || 5000,
   databaseUrl: process.env.DATABASE_URL || '',
-  shadowwDatabaseUrl: process.env.SHADOW_DATABASE_URL || '',
+  shadowDatabaseUrl: process.env.SHADOW_DATABASE_URL || '',
 };
