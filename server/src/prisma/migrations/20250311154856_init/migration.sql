89a53064b5d8f8e168056e418dd60195d8cbd20d
Frederik Lunde <frederik.a.lunde@gmail.com>
2025-03-11 16:49:24 +0100

    prisma migrate

---
+++ server/src/prisma/migrations/20250311154856_init/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE `users` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `username` VARCHAR(50) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    `created_at` TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+
+    UNIQUE INDEX `username`(`username`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
